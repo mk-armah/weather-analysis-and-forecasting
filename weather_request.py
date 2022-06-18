915176368f7cadee32b870a55ce5f0830fa6231e
mk-armah <mikeyarmah@gmail.com>
2022-06-18 21:47:38 +0000

    Updated Weather Request Functions

--- weather_request.py
+++ weather_request.py
@@ -1,3 +1,11 @@
+"""
+Author: Eng. Michael Kofi Armah
+Detail : Library for making weather requests
+Date Created : 4/1/22
+Last Modified : 6/18/22
+"""
+
+
 import numpy as np
 import datetime
 from datetime import timezone
@@ -8,20 +16,13 @@ import geopy
 from geopy.geocoders import Nominatim
 import pandas as pd
 
+from dotenv import dotenv_values
 
-# open weather api key
-with open("api_key.txt","r") as key:
-    key = key.read()
-
-url = "https://api.openweathermap.org/data/2.5/weather?q={cityname}\&appid={APIkey}&units=metric".format(cityname="Kumasi", APIkey=key)
-
-output = requests.get(url)
-
-data = output.json()
+credentials = dotenv_values(".env")
 
 
 # Initialize Nominatim AP
-def get_geocodes(place)-> geopy.location.Location:
+def get_geocodes(place) -> geopy.location.Location:
     """
     Gets the geographic cordinates of a given location
     pass location by name, e.g A city name like 'Accra'
@@ -60,19 +61,30 @@ def get_timestamp(y: int, m: int, d: int)->float:
 
 def make_request(lon:float, lat:float,
                 stamp: int, unit: str = "metric",
-                apikey: str = key) -> json:
+                apikey: str = credentials["APIKEY"]) -> tuple:
 
-    """get data for an entire day"""
-    url = "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={_lat_}&lon={_long_}&units={_unit_}&dt={_stamp_}&appid={_apikey_}".format(_lat_=lat, _long_=lon, _unit_=unit, _stamp_=int(stamp), _apikey_= str(apikey))
+    """get data for an entire day
+    Args:
+        lon (Longitude) | float: Longitudinal cordinate of the location under study
+        lat; (Latitude) | float: Latitudinal cordinate of the location under study
+        stamp (timestamp) | int: Utc Timestamp of the desired time 
+        apikey (API Key) | str: Open Weather Map API access key
+
+    Return:
+        json file containing the requested weather data
+        """
+
+    url = "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={_lat_}&lon={_long_}&units={_unit_}&dt={_stamp_}&appid={_apikey_}".format(_lat_=lat, _long_=lon, _unit_=unit, _stamp_=int(stamp), _apikey_= apikey)
 
     output = requests.get(url)
-    return output,output.json()
+    return output, output.json()
 
 
 def decode_Unix(timestamp)->str:
-    """convert timestands back to understandable time formats
+    """convert timestamps back to understandable time formats
     Arg:
         timestamp: datetime timestamp"""
+
     timestamp = datetime.datetime.fromtimestamp(timestamp)
     return timestamp.strftime('%Y-%m-%d %H:%M:%S')
 
@@ -81,7 +93,7 @@ def json_to_pandas(jsonfile: dict, dropna: bool = True) -> pd.DataFrame:
     """Extract Relevant Data from Json File and convert to Pandas DataFrame
 
     json:dict|json --> Input format json file format; the output after making a Request
-    dropna:bool    --> A boolean any[True,False] - removes all NaN's from the dataframe ; this will remove wind_gust and rain column
+    dropna:bool    --> A bool, any[True,False] - removes all NaN's from the dataframe if True; this will remove wind_gust and rain column
 
     Note : Not all columns in the Samples In the Json file have wind_gust and rain values"""
 
@@ -108,18 +120,12 @@ def json_to_pandas(jsonfile: dict, dropna: bool = True) -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    for city in ("Accra","Kumasi"):
-        for i in range(17-5,17):
-            ctime = datetime.datetime.now()
-            stamp = get_timestamp(y=ctime.year, m=ctime.month, d=ctime.day)
-            print("Decoded Stamp: ", decode_Unix(stamp))
-            location = get_geocodes(city)
-            output = make_request(location.longitude, location.latitude, stamp)
-            print(output)
-            temp_data = json_to_pandas(output)
-            if i==12 and city=="Accra":
-                df = temp_data
-            else:
-                df = pd.concat((temp_data,df))
-            print(stamp)
-
+    print(credentials["APIKEY"])
+    ctime = datetime.datetime.now()
+    stamp = get_timestamp(y=ctime.year, m=ctime.month, d=ctime.day)
+    print("Decoded Stamp: ", decode_Unix(stamp))
+    location = get_geocodes("Accra")
+    output = make_request(location.longitude, location.latitude, stamp)
+    print(output[1])
+    temp_data = json_to_pandas(output[1])
+    print(temp_data.head(24))
